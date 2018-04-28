7a83806bc28f0a17b8cfe92ef565b958fdda7c6e
Pete Werner <pewerner@comcast.net>
2018-04-28 08:12:52 -0400

    Updated Fun Facts

--- _site/Javascript/controller.js
+++ _site/Javascript/controller.js
@@ -7,7 +7,7 @@ bhccControllers.controller('NavController', function ($scope) {
 });
 
 
-bhccControllers.controller('StravaController', function ($scope, $http) {
+bhccControllers.controller('SegmentController', function ($scope, $http) {
 
     
 
@@ -20,6 +20,45 @@ bhccControllers.controller('StravaController', function ($scope, $http) {
     var req = {
         method: 'GET',
         url: 'https://bhcc-strava.herokuapp.com/bhcc/leaderboard.json',
+        headers: headers,
+        useXDomain: true,
+        dataType: "jsonp"
+        //data: { test: 'test' }
+    }
+
+
+
+        $http(req).then(function (data) {
+
+            $scope.StravaData = data.data
+
+            var d = ""
+
+        }, function (error) {
+
+            console.log(error, 'can not get data.');
+
+        })
+
+
+
+
+});
+
+
+bhccControllers.controller('FunFactController', function ($scope, $http) {
+
+    
+
+    var headers =  {
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, OPTIONS',
+        'Access-Control-Allow-Headers': 'Content-Type, X-Requested-With'
+    };
+
+    var req = {
+        method: 'GET',
+        url: 'https://bhcc-strava.herokuapp.com/bhcc/funFacts.json',
         headers: headers,
         useXDomain: true,
         dataType: "jsonp"
